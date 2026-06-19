04365d3e8fec3bdb4774cae62a92aa8ef8e6f0f0
Your Name <ssaiet.ritesh@gmail.com>
2026-06-19 12:28:57 +0530

    next config and package deps

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    unoptimized: true,
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*(svg|png|jpg|jpeg|gif|webp|ico)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
